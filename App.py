29b08ceb02a080fb8eae805ed4f0a4784803d333
nachoigs <nachoperegrino94@gmail.com>
2016-12-11 00:09:38 +0100

    Some improvements, graphics implemented

--- App.py
+++ App.py
@@ -93,10 +93,13 @@ def google():
 	for x in infolist:
 		if re.search('twitter.com/'+searchterm, x.link):
 			soup = BeautifulSoup(requests.get('http://twitter.com/' + searchterm).content, 'html5lib')
-			x.twitter_name = soup.find(class_="ProfileHeaderCard-nameLink").string if soup.find(class_="ProfileHeaderCard-nameLink") else x.twitter_name
-			x.twitter_location = soup.find(class_="ProfileHeaderCard-locationText u-dir").string if soup.find(class_="ProfileHeaderCard-locationText u-dir") else x.twitter_location
-			x.twitter_birth = soup.find(class_="ProfileHeaderCard-birthdateText u-dir").span.string if soup.find(class_="ProfileHeaderCard-birthdateText u-dir") else x.twitter_birth
-			x.twitter_bio = ''.join(text for text in soup.find(class_="ProfileHeaderCard-bio u-dir").find_all(string=True)) if soup.find(class_="ProfileHeaderCard-bio u-dir") else x.twitter_bio
+			## FALTA COGER LA URLIMG
+			twitter_name = soup.find(class_="ProfileHeaderCard-nameLink").string if soup.find(class_="ProfileHeaderCard-nameLink") else ""
+			twitter_location = soup.find(class_="ProfileHeaderCard-locationText u-dir").string if soup.find(class_="ProfileHeaderCard-locationText u-dir") else ""
+			twitter_birth = soup.find(class_="ProfileHeaderCard-birthdateText u-dir").span.string if soup.find(class_="ProfileHeaderCard-birthdateText u-dir") else ""
+			twitter_bio = ''.join(text for text in soup.find(class_="ProfileHeaderCard-bio u-dir").find_all(string=True)) if soup.find(class_="ProfileHeaderCard-bio u-dir") else ""
+
+			
 
 		if re.search("https://www.facebook.com/[\w\W]*"+firstterm, x.link, re.IGNORECASE):
 			soup = BeautifulSoup(requests.get(x.link).content, 'html5lib')
@@ -125,64 +128,231 @@ def tinfoleakfunction():
 
 	return render_template("tinfoleak.html", **templatedata)
 
-@app.route('/testlinkedin')
-def linkedintest():
-	link = request.args.get('link', '')
+@app.route('/testmultiple')
+def test():
+	link1 = request.args.get('link1', '') # Twitter
+	link2 = request.args.get('link2', '') # Facebook
+	link3 = request.args.get('link3', '') # Linkedin
 
-	if (link == ''):
+	if (link1 == '' and link2 == '' and link3 == ''):
 		return
 
 	infolist = []
-	infolist.append(info('', link, '' , ''))
+	infolist.append(info('', link1, '', ''))
+	infolist.append(info('', link2, '', ''))
+	infolist.append(info('', link3, '', ''))
 
 	for x in infolist:
-		linkedin_search(x, x.link)
+		if (x.link == link1):
+			twitter_search(x, x.link)
+		if (x.link == link2):
+			facebook_search(x, x.link)
+		if (x.link == link3):
+			linkedin_search(x, x.link)
 
-		for y in x.__dict__:
-			print('{}: {}'.format(y, x.__dict__[y]))
-			if isinstance(x.__dict__[y], list):
-				for z in x.__dict__[y]:
-					for t in z.__dict__:
-						print('{}: {}'.format(t, z.__dict__[t]))
-						if isinstance(z.__dict__[t], list):
-							for j in z.__dict__[t]:
-								for i in j.__dict__:
-									print('{}: {}'.format(i, j.__dict__[i].encode('utf-8')))
-
-
-
-						
+		# for y in x.__dict__:
+		# 	print('{}: {}'.format(y, x.__dict__[y]))
+		# 	if isinstance(x.__dict__[y], list):
+		# 		for z in x.__dict__[y]:
+		# 			for t in z.__dict__:
+		# 				print('{}: {}'.format(t, z.__dict__[t]))
+		# 				if isinstance(z.__dict__[t], list):
+		# 					for j in z.__dict__[t]:
+		# 						for i in j.__dict__:
+		# 							print('{}: {}'.format(i, j.__dict__[i].encode('utf-8')))					
 	return render_template("index.html", infolist=infolist)
-
-
 ## FUNCTIONS ##
 
+def search(searchterm):
+	infolist = []
 
-def twitter_search(x, searchterm):
-	soup = BeautifulSoup(requests.get('http://twitter.com/' + searchterm).content, 'html5lib')
-	x.twitter_name = soup.find(class_="ProfileHeaderCard-nameLink").string if soup.find(class_="ProfileHeaderCard-nameLink") else x.twitter_name
-	x.twitter_location = soup.find(class_="ProfileHeaderCard-locationText u-dir").string if soup.find(class_="ProfileHeaderCard-locationText u-dir") else x.twitter_location
-	x.twitter_birth = soup.find(class_="ProfileHeaderCard-birthdateText u-dir").span.string if soup.find(class_="ProfileHeaderCard-birthdateText u-dir") else x.twitter_birth
-	x.twitter_bio = ''.join(text for text in soup.find(class_="ProfileHeaderCard-bio u-dir").find_all(string=True)) if soup.find(class_="ProfileHeaderCard-bio u-dir") else x.twitter_bio
+	google_search(searchterm, infolist)
+
+	bing_search(searchterm, infolist)
+
+	firstterm = searchterm.split()[0]
+	secondterm = searchterm.split()[1]
+	for x in infolist:
+		if re.search("www.facebook.com/[\w\W]*"+firstterm, x.link, re.IGNORECASE):
+			facebook_search(x, x.link)
+
+		if re.search('twitter.com/'+searchterm, x.link):
+			twitter_search(x, x.link)
+
+		if re.search("linkedin.com/in/ignacio-saez-gomez-671a9855[\w\W]*"+firstterm, x.link, re.IGNORECASE)
+			linkedin_search(x, x.link)
+
+	return infolist
+
+
+def google_search(searchterm, infolist):
+	search = requests.get("https://www.googleapis.com/customsearch/v1?key=AIzaSyAivBHuR6rQLfFFzYZPsFKoW6NEu2uf178&cx=014899431841328080285:dgyb9nv0qd0&exactTerms={searchterm}&q=")
+	google = google.format(searchterm=searchterm)
+	googlejson = google.json()
+
+	for x in googlejson['items']:
+		link = x['link']
+		linkparsed = urlparse(link)
+		schemenetloc = linkparsed.scheme + "://" + linkparsed.netloc
+		text = x['snippet']
+
+		infolist.append(info(schemenetloc, link, text, ''))
+
+	return
+
+def bing_search(searchterm, infolist):
+	bingheader = {'Ocp-Apim-Subscription-Key': '9ed77606931144cca303b3bbffc23c63'}
+	binglink = "https://api.cognitive.microsoft.com/bing/v5.0/search?q=%%2B{searchterm}"
+	
+	searchterm = searchterm.replace(" ", "+")
+	binglink = binglink.format(searchterm=searchterm)
+	bing = requests.get(binglink, headers=bingheader)
+	bingprejson = bing.content.decode('utf-8')
+	bingjson = json.loads(bingprejson)
+	infolist = []
+
+	for x in bingjson['webPages']['value']:
+		link = x['displayUrl']
+		linkparsed = urlparse(link)
+		schemenetloc = linkparsed.scheme + "://" + linkparsed.netloc
+		text = x['snippet']
+		infolist.append(info(schemenetloc, link, text, ''))
+
+
+def twitter_search(x, link):
+	soup = BeautifulSoup(requests.get(link).content, 'html5lib')
+	profilecard = soup.find(class_="ProfileHeaderCard")
+
+	profilecard_name = profilecard.find(class_="ProfileHeaderCard-name")
+
+	twitter_urlimg = ''
+	twitter_name = ''
+	twitter_user = ''
+	twitter_bio = ''
+	twitter_location = ''
+	twitter_birth = ''
+	twitter_joindate = ''
+
+	profileavatar = soup.find(class_="ProfileAvatar")
+
+	if profileavatar:
+		twitter_urlimg = profileavatar.find(class_="ProfileAvatar-image")['src']
+
+	if profilecard_name:
+		twitter_name = profilecard_name.a.string
+
+	profilecard_user = profilecard.find(class_="ProfileHeaderCard-screenname")
+
+	if profilecard_user:
+		twitter_user = ''.join(profilecard_user.a.find_all(string=True))
+
+	profilecard_bio = profilecard.find(class_="ProfileHeaderCard-bio u-dir")
+
+	if profilecard_bio:
+		twitter_bio = ''.join(profilecard_bio.find_all(string=True))
+
+	profilecard_location = profilecard.find(class_="ProfileHeaderCard-location")
+
+	if profilecard_location:
+		twitter_location = profilecard_location.find(class_="ProfileHeaderCard-locationText").string
+
+	profilecard_birth = profilecard.find(class_="ProfileHeaderCard-birthdate")
+
+	if profilecard_birth:
+		twitter_birth = profilecard_birth.find(class_="ProfileHeaderCard-birthdateText").string
+
+	profilecard_joindate = profilecard.find(class_="ProfileHeaderCard-joinDate")
+
+	if profilecard_joindate:
+		twitter_joindate = profilecard_joindate.find(class_="ProfileHeaderCard-joinDateText")['title']
+
+	x.twitter_infos.append(twitter_info(twitter_urlimg, twitter_name, twitter_user, twitter_joindate, twitter_location, twitter_birth, twitter_bio))
 
 
 def facebook_search(x, link):
 	soup = BeautifulSoup(requests.get(x.link).content, 'html5lib')
-	x.facebook_location = soup.find(id='current_city').find(class_='_50f5 _50f7').string + '. ' if soup.find(id='current_city') else x.facebook_location
-	x.facebook_location = soup.find(id='hometown').find(class_='_50f5 _50f7').string + '. ' if soup.find(id='current_city') else x.facebook_location
 
-	for y in soup.find("", {"data-pnref":"edu"}).find_all(class_="_42ef"):
-		x.facebook_studies.append(' - '.join(y.find_all(string=True)))
+	facebook_bio = ""
+	facebook_name = ""
+	facebook_studies = []
+	facebook_works = []
+	facebook_urlimg = ""
+	facebook_locations = []
 
-	for y in soup.find("", {"data-pnref":"work"}).find_all(class_="_42ef"):
-		x.facebook_work.append(' - '.join(y.find_all(string=True)))
+	ProfileCover = soup.find(id="fbProfileCover")
+
+	if ProfileCover:
+		facebook_name = ProfileCover.find(id="fb-timeline-cover-name").string
+
+	ProfileAvatar = soup.find(class_="profilePicThumb")
+
+	if ProfileAvatar:
+
+		facebook_urlimg = soup.find(class_="profilePic img")['src']
+
+	page_eduwork = soup.find(id="pagelet_eduwork")
+
+	if page_eduwork:
+
+		page_work = page_eduwork.find(attrs={"data-pnref": "work"})
+
+		if page_work:
+			works = page_work.find_all(class_="fbEditProfileViewExperience")
+			for work in works:
+				title = page_work.find(class_="_2lzr _50f5 _50f7").a.string
+				subtitlediv = page_work.find(class_="fsm fwn fcg")
+				subtitle = ""
+				if subtitlediv:
+					subtitle = ' '.join(subtitlediv.find_all(string=True))
+				descriptiondiv = page_work.find(class_="_3-8w _50f8")
+				description = ""
+				if descriptiondiv:
+					description = description.string
+				facebook_works.append(facebook_work(title, subtitle, description))
+
+		page_edu = page_eduwork.find(attrs={"data-pnref": "edu"})
+
+		if page_edu:
+			studies = page_edu.find_all(class_="fbEditProfileViewExperience")
+			for study in studies:
+				school = study.find(class_="_2lzr _50f5 _50f7").a.string
+				school_location = study.find(class_="_173e _50f8 _50f3").div.string
+				facebook_studies.append(facebook_study(school, school_location))
+
+	page_hometown = soup.find(id="pagelet_hometown")
+
+	if page_hometown:
+
+		page_cities = page_hometown.find_all(class_="_3pw9 _2pi4 _2ge8")
+
+		for page_city in page_cities:
+
+			location = page_city.find(class_="_50f5 _50f7").a.string
+			state = page_city.find(class_="fsm fwn fcg").string
+
+			facebook_locations.append(facebook_location(location, state))
+
+		page_cities2 = page_hometown.find_all(class_="_43c8 _5f6p")
+
+		for page_city in page_cities2:
+
+			location = page_city.find(class_="_50f5 _50f7").a.string
+			state = page_city.find(class_="fsm fwn fcg").string
+
+			facebook_locations.append(facebook_location(location, state))
+
+	page_bio = soup.find(id="pagelet_bio")
+
+	if page_bio:
+		facebook_bio = page_bio.find(class_="_c24 _50f4").string
+
+	x.facebook_infos.append(facebook_info(facebook_urlimg, facebook_name, facebook_locations, facebook_studies, facebook_works, facebook_bio))
 
 
 def linkedin_search(x, link):
 	headers = {'User-agent': 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.1.4322)'}
 	r = requests.get(link, headers=headers)
 	soup = BeautifulSoup(r.content, 'html5lib')
-	print(link)
 
 	#print(soup.encode('utf-8'))
 
@@ -207,18 +377,18 @@ def linkedin_search(x, link):
 			subtitle = position.find(class_="item-subtitle").string or ""
 			date_range = "".join(position.find(class_="date-range").find_all(string=True)) or ""
 			location = position.find(class_="location").string or ""
+			description = position.find(class_="description").string or ""
+			experience_info.append(linkedin_experience(title, subtitle, date_range, location, description))
 
 
 	education = profile.find(id="education")
 	schools_info = []
 	if education:
-		schools = education.find_all("li", class_="school")
+		schools = education.find_all(class_="school")
 		for school in schools:
-			print(school.encode('utf-8'))
 			title = school.find(class_="item-title").string or ""
 			subtitle = school.find(class_="item-subtitle").find(class_="original translation").string or ""
-			period = " ".join(school.find(class_="date-range").find_all(string=True)) or ""
-			## Reminder: Check this, I have to try it ASAP ##
+			period = "".join(school.find(class_="date-range").find_all(string=True)) or ""
 			descriptionsoup = school.find(class_="description") or ""
 			description = " - "
 			if (descriptionsoup):
--- info.py
+++ info.py
@@ -6,14 +6,7 @@ class info:
         self.link = link
         self.pagetext = pagetext
         self.urlimg = urlimg
-        self.twitter_name = ''
-        self.twitter_age = ''
-        self.twitter_location = ''
-        self.twitter_birth = ''
-        self.twitter_bio = ''
-        self.facebook_location = ''
-        self.facebook_birthloc = ''
-        self.facebook_work = []
+        self.twitter_infos = []
         self.facebook_infos = []
         self.linkedin_infos = []
         type(self).counter += 1
@@ -21,49 +14,88 @@ class info:
     def __del__(self):
         type(self).counter -= 1
 
+class twitter_info:
+    def __init__(self, urlimg, name, user, joindate, location, birth, bio):
+        self.type = "Twitter"
+        self.urlimg = urlimg
+        self.name = name
+        self.user = user
+        self.joindate = joindate
+        self.location = location
+        self.birth = birth
+        self.bio = bio
+
+class facebook_info:
+    def __init__(self, urlimg, name, locations, studies, works, bio):
+        self.type = "Facebook"
+        self.name = name
+        self.urlimg = urlimg
+        self.locations = locations
+        self.studies = studies
+        self.works = works
+        self.bio = bio
+
+class facebook_work:
+    def __init__(self, title, subtitle, description):
+        self.title = title
+        self.subtitle = subtitle
+        self.description = description
+
+class facebook_study:
+    def __init__(self, school, school_location):
+        self.school = school
+        self.school_location = school_location
+
+class facebook_location:
+    def __init__(self, location, state):
+        self.location = location
+        self.state = state
+
 class linkedin_info:
-    def __init__(self, topcard='', experience='', schools='', awards='', languages='', certifications=''):
+    def __init__(self, topcard, experiences, schools, awards, languages, certifications):
+        self.type = "Linkedin"
         self.topcard = topcard
-        self.experience = experience
+        self.experiences = experiences
         self.schools = schools
         self.awards = awards
         self.languages = languages
         self.certifications = certifications
 
 class linkedin_topcard:
-    def __init__(self, picture='', name='', headline='', location='', sector=''):
-        self.picture = picture
-        self.name = name
+    def __init__(self, picture, name, headline, location, sector):
+        self.picture = picture #
+        self.name = name #
         self.headline = headline
         self.location = location
         self.sector = sector
 
 class linkedin_experience:
-    def __init__(self, title='', subtitle='', date_range='', location=''):
+    def __init__(self, title, subtitle, date_range, location, description):
         self.title = title
         self.subtitle = subtitle
         self.date_range = date_range
         self.location = location
+        self.description = description
 
 class linkedin_school:
-    def __init__(self, name='', studies='', period='', description=''):
+    def __init__(self, name, study, period, description):
         self.name = name
-        self.studies = studies
+        self.study = study
         self.period = period
         self.description = description
 
 class linkedin_awards:
-    def __init__(self, title='', subtitle='', description=' '):
+    def __init__(self, title, subtitle, description=' '):
         self.title = title
         self.subtitle = subtitle
         self.description = description
 
 class linkedin_languages:
-    def __init__(self, language='', proficiency=''):
+    def __init__(self, language, proficiency):
         self.language = language
         self.proficiency = proficiency
 
 class linkedin_certifications:
-    def __init__(self, title='', subtitle=''):
+    def __init__(self, title, subtitle):
         self.title = title
         self.subtitle = subtitle
