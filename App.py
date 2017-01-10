5d5714155332e37604cb3ca73841ee0ef85d2cfc
nachoigs <nachoperegrino94@gmail.com>
2017-01-10 01:48:55 +0100

    Data Base implemented

--- App.py
+++ App.py
@@ -8,6 +8,7 @@ from info import *
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 import tinfoleak as tinfoleak
+from flask_sqlalchemy import SQLAlchemy
 
 
 
@@ -15,103 +16,20 @@ import tinfoleak as tinfoleak
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\Users\\nacho_000\\Desktop\\Pagina con FLASK\\Database\\testdb.db' # Change for whatever path you want
 
+db = SQLAlchemy(app)
 
 @app.route('/')
-def hello_world():
+def index():
+	searchterm = request.args.get("search", "")
+
+	if (searchterm):
+		infolist = search(searchterm)
+
+		return render_template("index.html", infolist=infolist)
 
 	return render_template("index.html")
-
-@app.route('/user/<username>')
-def show_user_profile(username):
-	# show the user profile for that user
-	return 'User %s' % username
-
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-	# show the post with the given id, the id is an integer
-	return 'Post %d' % post_id
-
-@app.route('/about')
-def about():
-	return 'The about page ' + url_for('hello_world') + url_for('projects') + url_for('index', prueba='hello') + url_for('show_user_profile', username='prueba')
- 
-
-@app.route('/bing')
-def bing():
-	bingheader = {'Ocp-Apim-Subscription-Key': '9ed77606931144cca303b3bbffc23c63'}
-	binglink = "https://api.cognitive.microsoft.com/bing/v5.0/search?q=%%2B{searchterm}"
-	searchterm = request.args.get('search', '')
-	firstterm = searchterm.split()[0]
-	searchterm = searchterm.replace(" ", "+")
-	binglink = binglink.format(searchterm=searchterm)
-	bing = requests.get(binglink, headers=bingheader)
-	bingprejson = bing.content.decode('utf-8')
-	bingjson = json.loads(bingprejson)
-	infolist = []
-
-	for x in bingjson['webPages']['value']:
-		link = x['displayUrl']
-		linkparsed = urlparse(link)
-		schemenetloc = linkparsed.scheme + "://" + linkparsed.netloc
-		text = x['snippet']
-		infolist.append(info(schemenetloc, link, text, ''))
-
-
-	for x in infolist:
-		if re.search('twitter.com/'+searchterm, x.link):
-			twitter_search(x, searchterm)
-
-		if re.search("https://www.facebook.com/(?!public)[\w\W]*" + firstterm, x.link, re.IGNORECASE):
-			facebook_search(x, x.link)
-
-
-	# print(json.dumps(bingjson, sort_keys=True, indent=4, separators=(',', ': ')))
-
-	return render_template("index.html", infolist=infolist)
-
-
-@app.route('/google')
-def google():
-	# Uses Google Custom Search
-	google = requests.get("https://www.googleapis.com/customsearch/v1?key=AIzaSyAivBHuR6rQLfFFzYZPsFKoW6NEu2uf178&cx=014899431841328080285:dgyb9nv0qd0&exactTerms={searchterm}&q=")
-	searchterm = request.args.get('search', '')
-	google = google.format(searchterm=searchterm)
-	googlejson = google.json()
-
-	infolist = []
-
-	for x in googlejson['items']:
-		link = x['link']
-		linkparsed = urlparse(link)
-		schemenetloc = linkparsed.scheme + "://" + linkparsed.netloc
-		text = x['snippet']
-
-		infolist.append(info(schemenetloc, link, text, ''))
-
-	for x in infolist:
-		if re.search('twitter.com/'+searchterm, x.link):
-			soup = BeautifulSoup(requests.get('http://twitter.com/' + searchterm).content, 'html5lib')
-			## FALTA COGER LA URLIMG
-			twitter_name = soup.find(class_="ProfileHeaderCard-nameLink").string if soup.find(class_="ProfileHeaderCard-nameLink") else ""
-			twitter_location = soup.find(class_="ProfileHeaderCard-locationText u-dir").string if soup.find(class_="ProfileHeaderCard-locationText u-dir") else ""
-			twitter_birth = soup.find(class_="ProfileHeaderCard-birthdateText u-dir").span.string if soup.find(class_="ProfileHeaderCard-birthdateText u-dir") else ""
-			twitter_bio = ''.join(text for text in soup.find(class_="ProfileHeaderCard-bio u-dir").find_all(string=True)) if soup.find(class_="ProfileHeaderCard-bio u-dir") else ""
-
-			
-
-		if re.search("https://www.facebook.com/[\w\W]*"+firstterm, x.link, re.IGNORECASE):
-			soup = BeautifulSoup(requests.get(x.link).content, 'html5lib')
-			x.facebook_location = soup.find(id='current_city').a.string + '. ' if soup.find(id='current_city') else x.facebook_location
-
-			for y in soup.find("", {"data-pnref":"edu"}).find_all(class_="_42ef"):
-				x.facebook_studies.append(' - '.join(y.find_all(string=True)))
-
-			for y in soup.find("", {"data-pnref":"work"}).find_all(class_="_42ef"):
-				x.facebook_work.append(' - '.join(y.find_all(string=True)))
-
-	return render_template("index.html", infolist=infolist)
 
 @app.route('/tinfoleak')
 def tinfoleakfunction():
@@ -128,39 +46,6 @@ def tinfoleakfunction():
 
 	return render_template("tinfoleak.html", **templatedata)
 
-@app.route('/testmultiple')
-def test():
-	link1 = request.args.get('link1', '') # Twitter
-	link2 = request.args.get('link2', '') # Facebook
-	link3 = request.args.get('link3', '') # Linkedin
-
-	if (link1 == '' and link2 == '' and link3 == ''):
-		return
-
-	infolist = []
-	infolist.append(info('', link1, '', ''))
-	infolist.append(info('', link2, '', ''))
-	infolist.append(info('', link3, '', ''))
-
-	for x in infolist:
-		if (x.link == link1):
-			twitter_search(x, x.link)
-		if (x.link == link2):
-			facebook_search(x, x.link)
-		if (x.link == link3):
-			linkedin_search(x, x.link)
-
-		# for y in x.__dict__:
-		# 	print('{}: {}'.format(y, x.__dict__[y]))
-		# 	if isinstance(x.__dict__[y], list):
-		# 		for z in x.__dict__[y]:
-		# 			for t in z.__dict__:
-		# 				print('{}: {}'.format(t, z.__dict__[t]))
-		# 				if isinstance(z.__dict__[t], list):
-		# 					for j in z.__dict__[t]:
-		# 						for i in j.__dict__:
-		# 							print('{}: {}'.format(i, j.__dict__[i].encode('utf-8')))					
-	return render_template("index.html", infolist=infolist)
 ## FUNCTIONS ##
 
 def search(searchterm):
@@ -168,26 +53,36 @@ def search(searchterm):
 
 	google_search(searchterm, infolist)
 
-	bing_search(searchterm, infolist)
+	# bing_search(searchterm, infolist)
 
 	firstterm = searchterm.split()[0]
-	secondterm = searchterm.split()[1]
 	for x in infolist:
 		if re.search("www.facebook.com/[\w\W]*"+firstterm, x.link, re.IGNORECASE):
+			linkdb = relevants_links(searchterm, x.link, "Facebook" )
+			db.session.add(linkdb)
+			db.session.commit()
 			facebook_search(x, x.link)
 
 		if re.search('twitter.com/'+searchterm, x.link):
+			linkdb = relevants_links(searchterm, x.link, "Twitter" )
+			db.session.add(linkdb)
+			db.session.commit()
 			twitter_search(x, x.link)
 
-		if re.search("linkedin.com/in/ignacio-saez-gomez-671a9855[\w\W]*"+firstterm, x.link, re.IGNORECASE)
+		if re.search("linkedin.com/in/[\w\W]*"+firstterm, x.link, re.IGNORECASE):
+			linkdb = relevants_links(searchterm, x.link, "Linkedin" )
+			db.session.add(linkdb)
+			db.session.commit()
 			linkedin_search(x, x.link)
 
 	return infolist
 
 
 def google_search(searchterm, infolist):
-	search = requests.get("https://www.googleapis.com/customsearch/v1?key=AIzaSyAivBHuR6rQLfFFzYZPsFKoW6NEu2uf178&cx=014899431841328080285:dgyb9nv0qd0&exactTerms={searchterm}&q=")
-	google = google.format(searchterm=searchterm)
+	search = "https://www.googleapis.com/customsearch/v1?key=AIzaSyAivBHuR6rQLfFFzYZPsFKoW6NEu2uf178&cx=014899431841328080285:dgyb9nv0qd0&exactTerms={searchterm}&q="
+	search = search.format(searchterm=searchterm)
+	google = requests.get(search)
+
 	googlejson = google.json()
 
 	for x in googlejson['items']:
@@ -210,6 +105,7 @@ def bing_search(searchterm, infolist):
 	bingprejson = bing.content.decode('utf-8')
 	bingjson = json.loads(bingprejson)
 	infolist = []
+	print(bingjson)
 
 	for x in bingjson['webPages']['value']:
 		link = x['displayUrl']
@@ -426,3 +322,18 @@ def linkedin_search(x, link):
 
 	x.linkedin_infos.append(linkedin_info(topcard_info, experience_info, schools_info, awards_info, languages_info, certifications_info))
 
+# Models
+
+class relevants_links(db.Model):
+
+	id = db.Column('id', db.Integer, primary_key = True)
+	query = db.Column(db.String(100))
+	link = db.Column(db.String(100))
+	service = db.Column(db.String(50))
+
+	def __init__(self, query, link, service):
+		self.query = query
+		self.link = link
+		self.service = service
+
+db.create_all()
